1eacd6c0bc848138c4b3bd5338d69dfb10ff67ac
Pras00 <prasetiawahyu22@gmail.com>
2023-09-29 17:17:28 +0700

    login

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,37 @@
-fetch('http://localhost:4000/user')
-.then(response => response.json())
-.then(data => {
+const usernameInput = document.getElementById('usernameInput');
+const passwordInput = document.getElementById('passwordInput');
+const simpanDataButton = document.getElementById('login');
+
+
+simpanDataButton.addEventListener('click', () => {
+    fetch('http://localhost:4000/user')
+    .then(response => response.json())
+    .then(data => {
+
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+
+        const userData = data.data;
     
-    const username = "root";
-    const password = "root";
-
-    const userData = data.data;
-
-    const user = userData.find(user => user.username === username && user.password === password);
-
-    if (user) {
-        // Jika pengguna ditemukan, periksa peran (role)
-        if (user.role === "admin") {
-            // Redirect ke admin.html jika peran adalah admin
-            console.log("Dashboard Admin");
-        } else if (user.role === "user") {
-            // Redirect ke user.html jika peran adalah user
-            console.log('Home Page User');
+        const user = userData.find(user => user.username === username && user.password === password);
+    
+        if (user) {
+            // Jika pengguna ditemukan, periksa peran (role)
+            if (user.role === "admin") {
+                // Redirect ke admin.html jika peran adalah admin
+                window.location.href = "http://localhost:4000/admin"
+            } else if (user.role === "user") {
+                // Redirect ke user.html jika peran adalah user
+                window.location.href = "http://localhost:4000/home"
+            } else {
+                console.log("Peran tidak valid");
+            }
         } else {
-            console.log("Peran tidak valid");
+            console.log("Username atau password salah");
         }
-    } else {
-        console.log("Username atau password salah");
-    }
-
+    })
+    .catch(error => {
+    console.error('Error:', error);
+    });
 })
-.catch(error => {
-console.error('Error:', error);
-});
+
